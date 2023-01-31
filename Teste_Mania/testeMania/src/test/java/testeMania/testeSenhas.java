ff10f17e13aad1f549a2ac27d2a63c849c1eff54
Alerran Victor <alerransilva13@gmail.com>
2023-01-30 22:28:52 -0300

    UP

---
+++ Teste_Mania/testeMania/src/test/java/testeMania/testeSenhas.java
@@ -0,0 +1,62 @@
+package testeMania;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class testeSenhas {
+	private WebDriver driver;
+	
+	@Before
+	public void abrirNavegador() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+	
+	
+	@Test
+	public void testeNavegador() {
+		driver.get("http://localhost:4200/login");
+		
+		WebElement inputEmail = driver.findElement(By.id("email"));
+		WebElement inputSenha = driver.findElement(By.id("senha"));
+		WebElement botaoLogin = driver.findElement(By.id("botao-enviar"));
+		
+		
+		/*
+		inputEmail.sendKeys("alerran@email.com");
+		inputSenha.sendKeys("vip12345");
+		botaoLogin.click();
+		*/
+		
+		
+		String [] listaSenhas = {"senhaum", "senhadois", "senhaerrada", "outrasenhaerrada", "vip12345"};
+		
+		
+		for(int tentativas = 0; tentativas < listaSenhas.length; tentativas ++) {
+			try {
+				inputEmail.clear();
+				inputSenha.clear();
+				inputEmail.sendKeys("alerran@email.com");
+				inputSenha.sendKeys(listaSenhas[tentativas]);
+				botaoLogin.click();
+				Thread.sleep(3000);
+				
+			}
+			catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			
+			inputEmail.clear();
+			inputSenha.clear();
+			inputEmail.sendKeys("alerran@email.com");
+			inputSenha.sendKeys(listaSenhas[tentativas]);
+			botaoLogin.click();
+		}
+	}
+}
